9de40083c0acee0e4ca3f11480039bce75ddc634
wcyn <wasonga.cynthia@gmail.com>
2023-11-22 17:26:58 +0300

    Set up initial action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/sh -l
 
 # Use INPUT_<INPUT_NAME> to get the value of an input
-GREETING="Hello, $INPUT_WHO_TO_GREET!"
+START_MESSAGE="Using build platform: $INPUT_BUILD_PLATFORM"
 
 # Use workflow commands to do things like set debug messages
 echo "::notice file=entrypoint.sh,line=7::$GREETING"
+echo "$START_MESSAGE"
 
 # Write outputs to the $GITHUB_OUTPUT file
-echo "greeting=$GREETING" >> "$GITHUB_OUTPUT"
+# echo "greeting=$GREETING" >> "$GITHUB_OUTPUT"
 
 exit 0
